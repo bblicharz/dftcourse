ff83f159ce38b1ef84e3ff68d49858ce62b0dd07
Barbara Blicharz <blicharzbarbara3@gmail.com>
2021-04-21 15:04:51 +0200

    Add method routes

--- main.py
+++ main.py
@@ -6,3 +6,28 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/method")
+def method():
+    return {"method": "GET"}
+
+
+@app.delete("/method")
+def method():
+    return {"method": "DELETE"}
+
+
+@app.put("/method")
+def method():
+    return {"method": "PUT"}
+
+
+@app.options("/method")
+def method():
+    return {"method": "OPTIONS"}
+
+
+@app.post("/method", status_code=201)
+def method():
+    return {"method": "POST"}
