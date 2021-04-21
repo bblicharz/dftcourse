25930de91c0039847b2d2a0926847b0bb035675e
Barbara Blicharz <blicharzbarbara3@gmail.com>
2021-04-21 15:37:25 +0200

    Add register

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
 from fastapi import FastAPI, Response
 import hashlib
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -39,10 +42,41 @@ def auth(response: Response, password=None, password_hash=None):
     if password is None or password_hash is None:
         response.status_code = 401
         return response
-
+    # TODO
     hashed = hashlib.sha512(password.encode()).hexdigest()
     if hashed == password_hash:
         response.status_code = 204
     else:
         response.status_code = 401
     return response
+
+
+_DB = {}
+counter = 1
+
+class Person(BaseModel):
+    name: str
+    surname: str
+
+
+
+@app.post("/register", status_code=201)
+def register(person: Person):
+    global counter
+
+    curr_date = datetime.datetime.today()
+    vacc_date = curr_date + datetime.timedelta(days=1)
+
+    curr = curr_date.strftime('%Y-%m-%d')
+    vacc = vacc_date.strftime('%Y-%m-%d')
+    ret = {
+        "id": counter,
+        "name": person.name,
+        "surname": person.surname,
+        "register_date": curr,
+        "vaccination_date": vacc
+
+    }
+    counter += 1
+
+    return ret
