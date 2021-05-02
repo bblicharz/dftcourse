325f3d5e2b97904aaccbd6bc191d01351f5ef5ee
Barbara Blicharz <blicharzbarbara3@gmail.com>
2021-05-02 22:27:35 +0200

    Move old endpoints

---
+++ old.py
@@ -0,0 +1,95 @@
+
+@app.get("/")
+def root():
+    return {"message": "Hello world!"}
+
+
+@app.get("/method")
+def method():
+    return {"method": "GET"}
+
+
+@app.delete("/method")
+def method():
+    return {"method": "DELETE"}
+
+
+@app.put("/method")
+def method():
+    return {"method": "PUT"}
+
+
+@app.options("/method")
+def method():
+    return {"method": "OPTIONS"}
+
+
+@app.post("/method", status_code=201)
+def method():
+    return {"method": "POST"}
+
+
+@app.get("/auth")
+def auth(response: Response, password=None, password_hash=None):
+    if password is None or password_hash is None:
+        response.status_code = 401
+        return response
+    # TODO
+    hashed = hashlib.sha512(password.encode()).hexdigest()
+    if hashed == password_hash:
+        response.status_code = 204
+    else:
+        response.status_code = 401
+    return response
+
+
+_DB = {}
+counter = 1
+
+class Person(BaseModel):
+    name: str
+    surname: str
+
+
+
+@app.post("/register", status_code=201)
+def register(person: Person):
+    global _DB
+    global counter
+
+    daysnum = len(person.name) + len(person.surname)
+
+    curr_date = datetime.datetime.today()
+    vacc_date = curr_date + datetime.timedelta(days=daysnum)
+
+    curr = curr_date.strftime('%Y-%m-%d')
+    vacc = vacc_date.strftime('%Y-%m-%d')
+    ret = {
+        "id": counter,
+        "name": person.name,
+        "surname": person.surname,
+        "register_date": curr,
+        "vaccination_date": vacc
+
+    }
+
+    _DB[counter] = ret
+    counter = counter + 1
+
+    return ret
+
+
+@app.get("/patient/{item_id}")
+def patient(response: Response, item_id: int):
+    if item_id < 1:
+        response.status_code = 400
+        return response
+
+    global _DB
+
+    pat = _DB.get(item_id)
+    if pat is None:
+        response.status_code = 404
+        return response
+    else:
+        return pat
--- post.py
+++ post.py
@@ -1,38 +1,15 @@
-from datetime import date
-
-from fastapi import FastAPI, Response, HTTPException
-
 from hashlib import sha256
 from random import random
 
-from starlette.responses import HTMLResponse
+from fastapi import HTTPException, Response
 
-app = FastAPI()
+from main import app
 
 app.secret_key = "NotSoSecurePa$$"
 app.login = "4dm1n"
 app.access_tokens = []
 
-
-
-@app.get("/hello")
-async def read_items():
-    curr_date = date.today()
-    curr = curr_date.strftime('%Y-%m-%d')
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Hello! Today date is YYYY-MM-DD</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content.replace('YYYY-MM-DD', curr), status_code=200)
-
-
-@app.post("/login_session", status_code=201)
+@app.post("/login_session/", status_code=201)
 def login(user: str, password: str, response: Response):
     session_token = sha256(f"{user}{password}{app.secret_key}".encode()).hexdigest()
     app.access_tokens.append(session_token)
@@ -43,15 +20,13 @@ def login(user: str, password: str, response: Response):
     response.set_cookie(key="session_token", value=session_token)
     return response
 
-
-@app.post("/login_token", status_code=201)
+@app.post("/login_token/", status_code=201)
 def token(user: str, password: str):
     if user != app.login:
         raise HTTPException(status_code=401)
     if password != app.secret_key:
         raise HTTPException(status_code=401)
     return {"token": random.choice()}
-
 
 @app.get("/test")
 def test():
