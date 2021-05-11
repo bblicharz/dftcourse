188aa40395aa4db23fe5b20d904fa7f9dbc03e24
Barbara Blicharz <blicharzbarbara3@gmail.com>
2021-05-11 21:51:11 +0200

    Fixed decimals

--- main.py
+++ main.py
@@ -318,10 +318,16 @@ async def products_id_orders(response: Response, id: int):
                        "id": x['OrderID'],
                        "customer": x['CompanyName'],
                        "quantity": x["Quantity"],
-                       "total_price": "{:.2f}".format((x['UnitPrice'] * x['Quantity']) - (x['Discount'] * (x['UnitPrice'] * x['Quantity'])))
+                       "total_price": round((x['UnitPrice'] * x['Quantity']) - (x['Discount'] * (x['UnitPrice'] * x['Quantity'])))2
                    } for x in data
               ]
         }
+
+
+
+@app.post('/categories')
+async def categories(category = None):
+    category = {"name": "new test category"}
 
 
 
